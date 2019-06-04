da8dc1a1be2961f6bd9c83e7ec8a4745a778c1c6
Ryan Wolfe <rn.wolfe@gmail.com>
2019-06-04 16:45:13 -0400

    basic exec of 1+ command(s) to 1+ devices

--- app.js
+++ app.js
@@ -1,54 +1,65 @@
-node_ssh = require('node-ssh');
-// ssh = new node_ssh();
+const NetSSH = require('./index');
+const netssh = new NetSSH();
 
 const password = 'cisco';
 
-devices = [
-  { host: 'devcsr1.ironbowlab.com', username: 'admin', password: 'cisco', ssh: new node_ssh() },
-  { host: 'devcsr2.ironbowlab.com', username: 'admin', password: 'cisco', ssh: new node_ssh() },
-  { host: 'devcsr3.ironbowlab.com', username: 'admin', password: 'cisco', ssh: new node_ssh() },
-  { host: 'prodcsr1.ironbowlab.com', username: 'admin', password: 'cisco', ssh: new node_ssh() },
-  { host: 'prodcsr2.ironbowlab.com', username: 'admin', password: 'cisco', ssh: new node_ssh() },
-  { host: 'prodcsr3.ironbowlab.com', username: 'admin', password: 'cisco', ssh: new node_ssh() }
+const session = netssh.session({
+  host: 'devcsr1.ironbowlab.com',
+  username: 'admin',
+  password: password
+});
+
+// CASE: SEND ONE COMMAND TO ONE DEVICE
+// netssh.send_command('show run | i hostname', session).then(results => console.log(results));
+
+//CASE: SEND MULTIPLE COMMANDS TO ONE DEVICE
+// netssh
+//   .send_commands(['show run | i hostname', 'show clock'], session)
+//   .then(results => console.log(results));
+
+// CASE: SEND ONE COMMAND TO MULTIPLE DEVICES
+const devices = [
+  netssh.session({ host: 'devcsr1.ironbowlab.com', username: 'admin', password: password }),
+  netssh.session({ host: 'devcsr2.ironbowlab.com', username: 'admin', password: password }),
+  netssh.session({ host: 'devcsr3.ironbowlab.com', username: 'admin', password: password }),
+  netssh.session({ host: 'prodcsr1.ironbowlab.com', username: 'admin', password: password }),
+  netssh.session({ host: 'prodcsr2.ironbowlab.com', username: 'admin', password: password }),
+  netssh.session({ host: 'prodcsr3.ironbowlab.com', username: 'admin', password: password })
 ];
 
-let i = 0;
-let connections = [];
+const commands = [
+  'show run | i hostname',
+  'show clock',
+  'show ip route',
+  'show ntp status',
+  'show version',
+  'show ip eigrp neighbors',
+  'show cdp neighbors',
+  'show lldp neighbors'
+];
 
-devices.forEach(device =>
-  connections.push(
-    device.ssh.connect({
-      host: device.host,
-      username: device.username,
-      port: 22,
-      password: device.password,
-      tryKeyboard: true,
-      onKeyboardInteractive: (name, instructions, instructionsLang, prompts, finish) => {
-        if (prompts.length > 0 && prompts[0].prompt.toLowerCase().includes('password')) {
-          finish([device.password]);
-        }
-      }
-    })
-  )
-);
+// netssh.send_to_multi('show run | i hostname', devices).then(output => console.log(output));
 
-Promise.all(connections)
-  .then(results => {
-    connections = results;
-    console.log('connections theoretically completed..');
-    return Promise.all(
-      connections.map(conn => {
-        return conn.execCommand('show run | i hostname');
-      })
-    );
+// CASE: SEND MULTIPLE COMMANDS TO MULTIPLE DEVICES
+// netssh.send_commands_to_multi(commands, devices).then(output => handleOutput(output));
+
+// CASE: SEND CONFIG COMMANDS TO SINGLE DEVICE
+// Config commands don't work because of the single-command nature of this ssh lib
+// netssh
+//   .send_commands(['config t', 'ip route vrf mgmt-vrf 1.1.1.1 255.255.255.255 10.12.100.1'], session)
+//   .then(results => console.log(results));
+
+// CASE: SEND MULTIPLE CONFIG COMMANDS TO MULTIPLE DEVICES
+// Config commands don't work because of the single-command nature of this ssh lib
+
+// CASE: ONE COMMAND TO RULE THEM ALL
+const single = [
+  netssh.session({
+    host: 'devcsr1.ironbowlab.com',
+    username: 'admin',
+    password: password
   })
-  .then(output => output.forEach(out => console.log(out)))
-  .then(() => {
-    console.log('closing connections');
-    return Promise.all(connections.map(conn => conn.dispose()));
-  })
-  .catch(error => {
-    console.error(error);
-    console.log('closing connections');
-    return Promise.all(connections.map(conn => conn.dispose()));
-  });
+];
+
+netssh.send(commands, single).then(output => netssh.prettyOutput(output));
+// netssh.send(commands, devices).then(output => netssh.prettyOutput(output));
---
+++ index.js
@@ -0,0 +1,3 @@
+const NetSSH = require('./lib/netssh');
+
+module.exports = NetSSH;
